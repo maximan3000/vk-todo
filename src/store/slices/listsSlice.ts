8d09b252c72f9510fadd5c37aa8c9d2b53156a97
Максим Петров <maxim.petrov@formatkoda.ru>
2023-02-04 05:16:31 +0300

    refactor(store/slices): Refactored listsSlice.tsx

--- src/store/slices/listsSlice.ts
+++ src/store/slices/listsSlice.ts
@@ -9,97 +9,106 @@ import { fetchValue, putValue, LISTS_KEY } from 'shared/api';
 import { IStore } from 'store';
 import { assertLists } from 'store/asserts';
 
-export const listsSlice = createSlice<TLists, SliceCaseReducers<TLists>>({
-  name: 'lists',
-  initialState: [],
-  extraReducers(builder) {
-    builder.addMatcher(
-      (action) => action.type.endsWith('/fulfilled'),
-      (state, { payload }) => {
-        if (payload) return payload;
-      }
-    );
-  },
-  reducers: {},
-});
-
 export const fetchLists = createAsyncThunk(
   'lists/fetchLists',
-  async (): Promise<TLists> => {
-    const value = await fetchValue(LISTS_KEY);
-
-    return assertLists(value) ? value : [];
-  }
+  async (): Promise<unknown> => await fetchValue(LISTS_KEY)
 );
+
+const getLists = (getState: () => unknown) => (getState() as IStore).lists;
+
+const saveLists = async (lists: TLists): Promise<boolean> =>
+  putValue<TLists>(LISTS_KEY, lists);
 
 export const addList = createAsyncThunk(
   'lists/addList',
-  async (list: TList, { getState }): Promise<TLists> => {
-    const { lists } = getState() as IStore;
+  async (list: TList, { getState }): Promise<TList | null> => {
+    const lists = [...getLists(getState), list];
 
-    const _lists = JSON.parse(JSON.stringify(lists)) as TLists;
-
-    _lists.push(list);
-
-    const result = await putValue<TLists>(LISTS_KEY, _lists);
-    if (result) return _lists;
-    return [];
+    return (await saveLists(lists)) ? list : null;
   }
 );
 
 export const removeList = createAsyncThunk(
   'lists/removeList',
-  async (list: TList, { getState }): Promise<TLists> => {
-    const { lists } = getState() as IStore;
+  async (list: TList, { getState }): Promise<TList | null> => {
+    const lists = getLists(getState).filter(({ name }) => name !== list.name);
 
-    const _lists: TLists = JSON.parse(JSON.stringify(lists)) as TLists;
-
-    const listIndex = _lists.findIndex(({ name }) => name === list.name);
-    if (listIndex === -1) return [];
-    _lists.splice(listIndex, 1);
-
-    const result = await putValue<TLists>(LISTS_KEY, _lists);
-    if (result) return _lists;
-    return [];
+    return (await saveLists(lists)) ? list : null;
   }
 );
 
 export const addReminder = createAsyncThunk(
   'lists/addReminder',
-  async (reminder: TReminder, { getState }): Promise<TLists> => {
-    const { lists } = getState() as IStore;
+  async (reminder: TReminder, { getState }): Promise<TReminder | null> => {
+    const lists = getLists(getState).map((el) => {
+      if (el.name !== reminder.listName) return el;
 
-    const _lists: TLists = JSON.parse(JSON.stringify(lists)) as TLists;
+      return { ...el, reminders: el.reminders.concat(reminder) };
+    });
 
-    const list = _lists.find(({ name }) => name === reminder.listName);
-    if (!list) return [];
-    list.reminders.push(reminder);
-
-    const result = await putValue<TLists>(LISTS_KEY, _lists);
-    if (result) return _lists;
-    return [];
+    return (await saveLists(lists)) ? reminder : null;
   }
 );
 
 export const removeReminder = createAsyncThunk(
   'lists/removeReminder',
-  async (reminder: TReminder, { getState }): Promise<TLists> => {
-    const { lists } = getState() as IStore;
+  async (reminder: TReminder, { getState }): Promise<TReminder | null> => {
+    const lists = getLists(getState).map((el) => {
+      if (el.name !== reminder.listName) return el;
 
-    const _lists: TLists = JSON.parse(JSON.stringify(lists)) as TLists;
+      return {
+        ...el,
+        reminders: el.reminders.filter(({ name }) => name !== reminder.name),
+      };
+    });
 
-    const list = _lists.find(({ name }) => name === reminder.listName);
-    if (!list) return [];
-    const reminderIndex = list.reminders.findIndex(
-      ({ name }) => name === reminder.name
-    );
-    if (reminderIndex === -1) return [];
-    list.reminders.splice(reminderIndex, 1);
-
-    const result = await putValue<TLists>(LISTS_KEY, _lists);
-    if (result) return _lists;
-    return [];
+    return (await saveLists(lists)) ? reminder : null;
   }
 );
+
+export const listsSlice = createSlice<TLists, SliceCaseReducers<TLists>>({
+  name: 'lists',
+  initialState: [],
+  extraReducers(builder) {
+    builder.addCase(fetchLists.fulfilled, (state, { payload }) => {
+      if (payload && assertLists(payload)) {
+        return payload;
+      }
+    });
+
+    builder.addCase(addList.fulfilled, (state, { payload }) => {
+      if (payload) {
+        state.push(payload);
+      }
+    });
+
+    builder.addCase(removeList.fulfilled, (state, { payload }) => {
+      if (payload) {
+        state = state.filter(({ name }) => name !== payload.name);
+      }
+    });
+
+    builder.addCase(addReminder.fulfilled, (state, { payload }) => {
+      if (payload) {
+        state
+          .find((list) => list.name === payload.listName)
+          ?.reminders.push(payload);
+      }
+    });
+
+    builder.addCase(removeReminder.fulfilled, (state, { payload }) => {
+      if (payload) {
+        const list = state.find((list) => list.name === payload.listName);
+
+        if (!list) return state;
+
+        list.reminders = list?.reminders.filter(
+          ({ name }) => name !== payload.name
+        );
+      }
+    });
+  },
+  reducers: {},
+});
 
 export default listsSlice.reducer;
